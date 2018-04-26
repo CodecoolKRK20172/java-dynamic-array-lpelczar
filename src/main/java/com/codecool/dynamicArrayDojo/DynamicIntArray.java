bc7dd970b4fb37469a4802f77d25ede68b29a743
lpelczar <lukepelczar@gmail.com>
2018-04-26 14:07:28 +0200

    Change variable names

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -6,25 +6,24 @@ import java.util.stream.IntStream;
 class DynamicIntArray {
 
     private int[] array;
-    private int actualSize = 0;
+    private int size = 0;
 
     DynamicIntArray() {
-        int initialSize = 5;
-        this.array = new int[initialSize];
+        this.array = new int[0];
     }
 
-    DynamicIntArray(int initialSize) {
-        this.array = new int[initialSize];
+    DynamicIntArray(int capacity) {
+        this.array = new int[capacity];
     }
 
     void add(int number) {
-        if (actualSize < array.length) {
-            array[actualSize] = number;
-            actualSize++;
+        if (size < array.length) {
+            array[size] = number;
+            size++;
         } else {
             this.array = Arrays.copyOf(array, array.length + 1);
-            array[actualSize] = number;
-            actualSize++;
+            array[size] = number;
+            size++;
         }
     }
 
@@ -35,7 +34,7 @@ class DynamicIntArray {
         int[] firstHalf = Arrays.copyOfRange(array, 0, index);
         int[] secondHalf = Arrays.copyOfRange(array, index + 1, array.length);
         this.array = IntStream.concat(Arrays.stream(firstHalf), Arrays.stream(secondHalf)).toArray();
-        actualSize--;
+        size--;
     }
 
     void insert(int index, int value) {
@@ -48,7 +47,7 @@ class DynamicIntArray {
             int[] secondHalf = Arrays.copyOfRange(array, index, array.length);
             int[] firstWithValue = IntStream.concat(Arrays.stream(firstHalf), Arrays.stream(new int[]{value})).toArray();
             this.array = IntStream.concat(Arrays.stream(firstWithValue), Arrays.stream(secondHalf)).toArray();
-            actualSize++;
+            size++;
         }
     }
 
