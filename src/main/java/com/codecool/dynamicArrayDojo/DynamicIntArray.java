52c7b1fc3228d6b56cc300937262f9bab0e41a83
lpelczar <lukepelczar@gmail.com>
2018-04-26 13:11:00 +0200

    Implement add method

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,11 +1,39 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
+import java.util.Arrays;
+
 class DynamicIntArray {
 
     private int[] array;
+    private int actualSize = 0;
+
+    DynamicIntArray() {
+        int initialSize = 5;
+        this.array = new int[initialSize];
+    }
 
     DynamicIntArray(int initialSize) {
         this.array = new int[initialSize];
+    }
+
+    void add(int number) {
+        if (actualSize < array.length) {
+            array[actualSize] = number;
+            actualSize++;
+        } else {
+            this.array = Arrays.copyOf(array, array.length + 1);
+            array[actualSize] = number;
+            actualSize++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int number : array) {
+            result.append(" ");
+            result.append(number);
+        }
+        return result.toString();
     }
 }
